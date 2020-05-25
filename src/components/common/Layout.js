9062f03acd61e182148f464246cbb1d3d775b357
emTr0 <em@emtr0.com>
2020-05-25 10:50:22 -0700

    Update Layout.js

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -65,7 +65,7 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                                     <Navigation data={site.navigation} navClass="site-nav-item" />
                                 </div>
                                 <div className="site-nav-right">
-                                    <Link className="site-nav-button" to="/about">About</Link>
+                                    <Link className="site-nav-button" to="/about/">About</Link>
                                 </div>
                             </nav>
                         </div>
