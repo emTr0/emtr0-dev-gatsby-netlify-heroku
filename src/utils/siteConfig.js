9915bf2c47658ae020977489a27632e00df70bcd
emTr0 <em@emtr0.com>
2020-05-26 07:12:35 -0700

    Update siteConfig.js

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: `https://emtr0-dev.netlify.app`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://emtr0.dev`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
